57759ab9ddf034e6daca224b3761b1499ab71070
NovaKal <kevin.alejandro.vasco@gmail.com>
2021-08-24 17:24:03 -0500

    Feature 1

--- main.c
+++ main.c
@@ -4,5 +4,24 @@
 
 int main(int argc, char *argv[]){
 
+    FILE *inFile = fopen("./fin", "r");
+    FILE *inFileOut = fopen("./fout", "a+");
+    if (inFile == NULL) {
+        perror("Open file fin fails: ");
+        return(EXIT_FAILURE);
+    }
+    if (inFileOut == NULL) {
+        perror("Open file fout fails: ");
+        return(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < 6; i++)
+    {
+        if (i == 1) feature1(inFile, inFileOut);
+    }
+    
+    fclose(inFile);
+    fclose(inFileOut);
+    
     return EXIT_SUCCESS;
 }
--- student.c
+++ student.c
@@ -1,3 +1,16 @@
 #include <stdlib.h>
 #include "student.h"
 
+void feature1(FILE *fin, FILE *fout) {
+    
+    char buffer[64];
+    char *status = NULL;
+
+    status = fgets(buffer, sizeof(buffer), fin);
+    if (status != NULL) {
+        fprintf(fout, "%s", buffer);
+    }
+    printf("\n");
+
+    fflush(fin);
+}
