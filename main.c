da1d2cecc5af87f8b97fe0d1e3313513236ed508
Kevin <kevin.alejandro.vasco@gmail.com>
2021-09-02 16:07:51 -0500

    New Feature 3

--- main.c
+++ main.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "student.h"
 
 int main(int argc, char *argv[]){
 
-    FILE *inFile = fopen("./fin", "r");
-    FILE *inFileOut = fopen("./fout", "a+");
+    if (argc == 1 || argc > 4) {
+        perror("La cantidad de archivos necesarios para la ejecucion es incorrecta.");
+        return(EXIT_FAILURE);
+    }
+    FILE *inFile = fopen(argv[1], "r");
+    FILE *outFile = fopen("./fout", "a+");
+
     if (inFile == NULL) {
         perror("Open file fin fails: ");
         return(EXIT_FAILURE);
     }
-    if (inFileOut == NULL) {
+
+    if (outFile == NULL) {
         perror("Open file fout fails: ");
         return(EXIT_FAILURE);
     }
 
-    for (int i = 0; i < 6; i++)
-    {
-        if (i == 1) feature1(inFile, inFileOut);
-        if (i == 2) feature2(inFile, inFileOut);
-        if (i == 3) feature3(inFile, inFileOut);
 
-    }
-    
+    feature1(inFile, outFile);
+    feature2(inFile, outFile);
+    feature3(inFile, outFile);
+
     fclose(inFile);
-    fclose(inFileOut);
+    fclose(outFile);
     
     return EXIT_SUCCESS;
 }
--- student.c
+++ student.c
@@ -4,63 +4,39 @@
 #include "student.h"
 
 void feature1(FILE *fin, FILE *fout) {
-    
-    char buffer[64];
+    char buffer[128];
     char *status = NULL;
-
     status = fgets(buffer, sizeof(buffer), fin);
-
     if (status != NULL) {
         fprintf(fout, "%s", buffer);
     }
-
-    printf("\n");
-    fflush(fin);
 }
 
 void feature2(FILE *fin, FILE *fout) {
-    
-    char buffer[70];
+    char buffer[128];
     char *status = NULL;
-
     status = fgets(buffer, sizeof(buffer), fin);
-
     if (status != NULL) {
-
-        for (int i = sizeof(buffer) - 2; i >= 0; i--) {
+        int length = strlen(buffer);
+        for (int i = length - 2; i >= 0; i--) {
             fprintf(fout, "%c", buffer[i]);
         }
-
-        fprintf(fout, "%s", "\n");
     }
-    
-    printf("\n");
-    fflush(fin);
 }
 
 void feature3(FILE *fin, FILE *fout) {
-    
-    char buffer[64];
+    char buffer[128];
+    char *status = NULL;
     char *endptr;
     char *saveEndptr;
-    char *status = NULL;
-    int suma = 0;
-
-    //Aui va mensaje para el profesor. El mensaje como tal esta en el commit del feature 3.
     status = fgets(buffer, sizeof(buffer), fin);
-    status = fgets(buffer, sizeof(buffer), fin);
-
     if (status != NULL) {
-
+        int suma = 0;
         saveEndptr = buffer;
-
         while (*saveEndptr != 0x0a) {
             suma += strtol(saveEndptr, &endptr, 10);
             saveEndptr = endptr;
         }
-
-        fprintf(fout, "%d", suma);
+        fprintf(fout, "\n%d", suma);
     }
-    printf("\n");
-    fflush(fin);
 }
